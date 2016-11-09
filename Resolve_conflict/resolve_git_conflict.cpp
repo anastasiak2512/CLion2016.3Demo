ff46fcd5ca82b85700e3d545d6599ec60ed7ba6a
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2016-11-09 17:09:08 +0300

    Resolve conflict sample

---
+++ Resolve_conflict/resolve_git_conflict.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+typedef struct PersonData {
+    int age;
+    int id;
+    const char *name;
+
+    friend std::ostream &operator<<(std::ostream &os , const PersonData &data);
+} PersonData;
+
+std::ostream &operator<<(std::ostream &os , const PersonData &data)
+{
+    os << "id: " << data.id << " name: " << data.name;
+    return os;
+}
+
+void printing() {
+    PersonData p = {64, 0, "Bjarne Stroustrup"};
+    std::cout << p << std::endl;
+}
