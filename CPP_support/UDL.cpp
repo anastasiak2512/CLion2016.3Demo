e3317bb36d3b3db939eabc9c868ef1e76db84a9c
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2016-11-09 16:42:55 +0300

    Updated sample help

--- CPP_support/UDL.cpp
+++ CPP_support/UDL.cpp
@@ -5,7 +5,7 @@ constexpr long double operator"" _fah(long double const degrees)
     return (degrees - 32.0) / 1.8;
 }
 
-// Call quick documentation (Ctrl+Q/F1) on _fah
+// Call quick documentation (Ctrl+Q on Windows and Linux / F1 on macOS) on _fah
 // Rename _fah -> _fh using Rename refactoring
 bool checkTemperature(long double temp) {
     auto threshold = 77.0_fah;
