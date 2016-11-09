a411e8baa6fc8fc00f696002424fd5ccaad32c03
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2016-11-09 18:20:28 +0300

    Font with ligatures samples

---
+++ font_with_ligatures/font_with_ligatures.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void font_test() {
+    map<string, string> it_map;
+
+    it_map.insert({"Ada", "Ada Lovelace"});
+    it_map.insert({"Turing Machine", "Alan Turing"});
+    it_map.insert({"Lisp", "John McCarthy"});
+    it_map.insert({"C++", "Bjarne Stroustrup"});
+    it_map.insert({"GNU project", "Richard Stallman"});
+    it_map.insert({"Linux", "Linus Torvalds"});
+
+    auto search = it_map.find("C++");
+    if (search != it_map.end()) {
+        std::cout << "Found " << search->first <<
+                  " created by " << search->second << '\n';
+    }
+    else {
+        std::cout << "Not found\n";
+    }
+}
