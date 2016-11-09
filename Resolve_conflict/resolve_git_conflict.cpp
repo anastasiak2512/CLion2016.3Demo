1130b32daebcda596bc6de773f53c355b090b67f
anastasiak2512 <anastasia.kazakova2512@gmail.com>
2016-11-09 18:15:25 +0400

    Add print flag
    Add flag parameter to printing function

--- Resolve_conflict/resolve_git_conflict.cpp
+++ Resolve_conflict/resolve_git_conflict.cpp
@@ -8,7 +8,7 @@ typedef struct PersonData {
     friend std::ostream &operator<<(std::ostream &os , const PersonData &data);
 } PersonData;
 
-std::ostream &operator<<(std::ostream &os , const PersonData &data)
+std::ostream &operator<<(std::ostream &os , const PersonData &data, bool print_flag)
 {
     os << "id: " << data.id << " name: " << data.name;
     return os;
