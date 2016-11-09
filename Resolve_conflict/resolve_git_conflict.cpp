727a576a09e81bf2ee1081d13e074ed5192ace2c
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2016-11-09 17:45:55 +0300

    Help message added

--- Resolve_conflict/resolve_git_conflict.cpp
+++ Resolve_conflict/resolve_git_conflict.cpp
@@ -6,6 +6,11 @@ private:
     int id;
     const char *name;
 
+    //Introduce nonoverlapping changes on one line.
+    //For example, remove public identifier externally
+    //and change function parameter in the IDE. Try to push
+    //and get a special resolve icon in the merge dialog.
+    //Click the icon to have the conflict resolved by the IDE.
 public:
     void printing(int type) {
         if (type) {
